dc8f5536c6ce26a29725d5c253747a23642e41c9
poya <rains9309@gmail.com>
2015-01-14 18:24:03 +0800

    delete

--- content-none.php
+++
@@ -1,8 +0,0 @@
-<?php  
-/**
- * The template part for displaying a message that posts cannot be found.
- *
- * @package classPlus
- */
-?>
-<h3 class="page-title"><?php _e('No Posts Found.', 'twenty-theme'); ?></h3>
